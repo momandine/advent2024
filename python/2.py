6ddbe3d04e58e023ead53406ce78cdcdaa8abe95
Amandine Lee <momandine@users.noreply.github.com>
2024-12-11 22:23:54 -0800

    Add typing, do part 4 star 1

--- python/2.py
+++ python/2.py
@@ -2,18 +2,18 @@ from input import slurp
 
 reports = [[int(x) for x in row.split(' ')] for row in slurp(2).splitlines()]
 
-def is_monotonic(levels):
+def is_monotonic(levels: str) -> bool:
     sorted_levels = sorted(levels)
     return sorted_levels == levels or sorted_levels == levels[::-1]
 
-def check_gaps(levels):
+def check_gaps(levels: str) -> bool:
     return all(4 > abs(levels[i] - levels[i + 1]) > 0 for i in range(len(levels) - 1))
 
 # Original
 print(sum([is_monotonic(report) and check_gaps(report) for report in reports]))
 
 
-def check_with_eliminates(rep):
+def check_with_eliminates(rep: str) -> bool:
     if is_monotonic(rep) and check_gaps(rep):
         return True
     for i in range(len(rep)):
--- python/3.py
+++ python/3.py
@@ -1,5 +1,5 @@
 import re
-import pdb
+from typing import List
 
 from input import slurp
 
@@ -9,12 +9,12 @@ do_chunks = text.split("do()")
 do_these = [chunk.split("don't()")[0] for chunk in do_chunks]
 minus_donts = "".join(do_these)
 
-def evalute(mult_str: str):
+def evalute(mult_str: str) -> int:
     nums = re.findall("\d{1,3}", mult_str)
     assert len(nums) == 2
     return int(nums[0]) * int(nums[1])
 
-def find_multiplications(string: str):
+def find_multiplications(string: str) -> List[str]:
     to_match = "mul\(\d{1,3},\d{1,3}\)"
     return re.findall(to_match, string)
 
