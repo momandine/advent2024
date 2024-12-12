d6e3dff3e624a913cc831a6cdbcc46221c5a7099
Amandine Lee <momandine@users.noreply.github.com>
2024-12-11 21:36:35 -0800

    day 3

--- python/3.py
+++ python/3.py
@@ -1,4 +1,21 @@
 import re
+import pdb
 
+from input import slurp
 
+text = slurp(3)
 
+do_chunks = text.split("do()")
+do_these = [chunk.split("don't()")[0] for chunk in do_chunks]
+minus_donts = "".join(do_these)
+
+def evalute(mult_str: str):
+    nums = re.findall("\d{1,3}", mult_str)
+    assert len(nums) == 2
+    return int(nums[0]) * int(nums[1])
+
+def find_multiplications(string: str):
+    to_match = "mul\(\d{1,3},\d{1,3}\)"
+    return re.findall(to_match, string)
+
+print(sum([evalute(match) for match in find_multiplications(minus_donts)]))
