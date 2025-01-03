ef5797b63f304c777d833b9117e01a80dfc64858
Amandine Lee <momandine@users.noreply.github.com>
2025-01-02 16:46:43 -0800

    Move all inputs to a directory

--- python/input.py
+++ python/input.py
@@ -2,7 +2,7 @@ import os
 
 def slurp(day: int):
     input_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    filename = os.path.join(input_dir, f'input{day}.txt')
+    filename = os.path.join(input_dir, 'inputs', f'input{day}.txt')
 
 
     with open(filename, 'r') as file:
